84b5065bb7d576dc64987c1ec87ae752f4f8598d
Donovan Hiland <donovan.hiland@gmail.com>
2016-04-02 11:06:50 -0700

    Finished PRACTICE PROBLEMS

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -6,6 +6,14 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+function isTyler(name) {
+  if (name === "Tyler") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 //Next problem
 
@@ -15,7 +23,10 @@ var name = 'Tyler';
 
 
   //Code Here
-
+function getName() {
+  var name = prompt("Enter name");
+  return name;
+}
 
 //Next Problem
 
@@ -25,7 +36,10 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+function welcome() {
+  var name = getName();
+  alert("Welcome, " + name);
+}
 
 //Next problem
 
@@ -35,7 +49,7 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
-
+// parameters are variables and arguments are the values supplied by the user in the parameter place.
 
 //Next problem
 
@@ -45,7 +59,7 @@ var name = 'Tyler';
 
 
   //Answer Here
-
+// 0, "", undefined, NaN, null, false
 
 
 //Next Problem
@@ -55,16 +69,17 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+function myName() {
+  return "Donovan";
+}
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = myName;
 //Now alert the result of invoking newMyName
-
-
+alert(newMyName());
 
 //Next problem
 
@@ -73,9 +88,15 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn() {
+  var myNameFn = function() {
+    return "Donovan";
+  }
+  return myNameFn;
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
